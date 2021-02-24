9e2530cd9a688ccc756c279436d16158c9769565
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-02-24 10:43:52 -0600

    delete web routes and set api routs by default

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function () {
+    return 'BooksApi';
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=>'books'], function () {
+
+    Route::get('/',[BookController::class,'index']);
+    Route::post('/',[BookController::class,'store']);
+    Route::get('/{book}',[BookController::class,'show']);
+    Route::put('/{book}',[BookController::class,'update']);
+    Route::patch('/{book}',[BookController::class,'update']);
+    Route::delete('/{book}',[BookController::class,'destroy']);
+
 });
